6735bc79407ba0986fe6c3108df30b2548b0442e
bjoarder7 <bjoarder87@gmail.com>
2024-12-06 08:48:13 +0600

    setup packages setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-import express from 'express'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 
+const app: Application = express();
 
-const app = express()
-const port = 3000
+// parsers
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (req: Request, res: Response) => {
+  let a = 10;
+  res.send(a);
+});
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send('Hello Phero site!');
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
-import { Server } from 'http';
 import app from './app'
+import config from './app/config';
+import mongoose from 'mongoose';
 
-const PORT =5000;
 
-let server: Server;
+async function main() {
+  try{
+    await mongoose.connect(config.database_url as string);
 
-async function bootstrap(){
-    server = app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`)
-      })
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch(err){
+    console.log(err)
+  }
 }
 
-bootstrap();
+main();
+
+
+
+
 
